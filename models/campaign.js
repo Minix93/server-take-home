defca7fe1b6bd096be1828f9fa3be43b4e58887c
Minix93 <mxjason93@gmail.com>
2022-10-04 12:59:26 -0700

    1. created model : campaign.js with coloumns : "name", "icon_url" that maps to the campaign's name and icon image

---
+++ models/campaign.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+    var Campaign = sequelize.define(
+        'Campaign',
+        {
+            name: { type: DataTypes.STRING },
+            icon_url: { type: DataTypes.STRING },
+        },
+        {
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+            deletedAt: 'deleted_at',
+            paranoid: true,
+            underscored: true,
+            tableName: 'campaign',
+        },
+    );
+
+    return Campaign;
+};
