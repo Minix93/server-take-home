cb0865cdf2fd8656264c78f239587259ff298eaf
Minix93 <mxjason93@gmail.com>
2022-10-04 13:24:40 -0700

    1. set up the relationship between campaign and creator to (Many to Many)

--- models/index.js
+++ models/index.js
@@ -8,6 +8,7 @@ if (!global.hasOwnProperty('db')) {
         sequelize: sequelize,
         Creator: require(__dirname + '/creator')(sequelize, Sequelize.DataTypes),
         Install: require(__dirname + '/install')(sequelize, Sequelize.DataTypes),
+        // new models add below
         Campaign: require(__dirname + '/campaign')(sequelize, Sequelize.DataTypes),
         Price: require(__dirname + '/price')(sequelize, Sequelize.DataTypes),
         Media: require(__dirname + '/media')(sequelize, Sequelize.DataTypes)
@@ -33,4 +34,23 @@ if (!global.hasOwnProperty('db')) {
     * TODO add any additional relationships between models here.
     *
     */
+
+    // new relationships added below:
+
+    /**
+     * Creator and Campaign --> Many to Many
+     */
+
+    global.db.Creator.belongsToMany(db.Campaign, {
+        through: "creator_campaign",
+        as: "campaigns",
+        foreignKey: "campaign_id"
+    });
+    global.db.Campaign.belongsToMany(db.Creator, {
+        through: "creator_campaign",
+        as: "creators",
+        foreignKey: "creator_id"
+    });
+
+
 }
