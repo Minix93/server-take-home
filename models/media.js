07adb1cec3b805c0dbb222bf2c8532b5e8864b1f
Minix93 <mxjason93@gmail.com>
2022-10-04 13:05:48 -0700

    1. created model : media.js with coloumns : "media_type", "media_url"

---
+++ models/media.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+    var Media = sequelize.define(
+        'Media',
+        {
+            media_type: { type: DataTypes.STRING },
+            media_url: { type: DataTypes.STRING },
+        },
+        {
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+            deletedAt: 'deleted_at',
+            paranoid: true,
+            underscored: true,
+            tableName: 'media',
+        },
+    );
+
+    return Media;
+};
